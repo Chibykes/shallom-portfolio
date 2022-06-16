0e21df5a0a82eaa8fd62984f0e93d8196649f309
Chibykes <62060851+Chibykes@users.noreply.github.com>
2022-06-16 11:10:35 +0100

    UI Spacing added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -137,9 +137,9 @@ function Home() {
                                     <p className="font-[500] lg:text-lg text-zinc-400">
                                         {summary}
                                     </p>
-                                    <div className="lg:px-10 lg:py-5 px-8 py-4 inline-flex justify-center items-center gap-x-5 rounded-lg bg-main-black text-white">
-                                        <p className="font-semibold">View Case Study</p>
-                                        <svg className="scale-75" width="51" height="16" viewBox="0 0 51 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <div className="lg:px-10 lg:py-5 px-6 py-4 inline-flex justify-center items-center lg:gap-x-5 gap-x-3 lg:rounded-lg rounded-md bg-main-black text-white">
+                                        <p className="font-semibold text-sm">View Case Study</p>
+                                        <svg className="lg:scale-75 scale-50" width="51" height="16" viewBox="0 0 51 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M50.7071 8.70711C51.0976 8.31658 51.0976 7.68342 50.7071 7.29289L44.3431 0.928932C43.9526 0.538408 43.3195 0.538408 42.9289 0.928932C42.5384 1.31946 42.5384 1.95262 42.9289 2.34315L48.5858 8L42.9289 13.6569C42.5384 14.0474 42.5384 14.6805 42.9289 15.0711C43.3195 15.4616 43.9526 15.4616 44.3431 15.0711L50.7071 8.70711ZM0 9H50V7H0V9Z" fill="white"/>
                                         </svg>
 
@@ -171,9 +171,9 @@ function Home() {
                                 <p className="font-[500] lg:text-lg text-zinc-600">
                                     {summary}
                                 </p>
-                                <div className="lg:px-10 lg:py-5 px-8 py-4 inline-flex justify-center items-center gap-x-5 rounded-lg bg-main-black text-white">
-                                    <p className="font-semibold">View Case Study</p>
-                                    <svg className="scale-75" width="51" height="16" viewBox="0 0 51 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <div className="lg:px-10 lg:py-5 px-6 py-4 inline-flex justify-center items-center lg:gap-x-5 gap-x-3 lg:rounded-lg rounded-md bg-main-black text-white">
+                                    <p className="font-semibold text-sm">View Case Study</p>
+                                    <svg className="lg:scale-75 scale-50" width="51" height="16" viewBox="0 0 51 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M50.7071 8.70711C51.0976 8.31658 51.0976 7.68342 50.7071 7.29289L44.3431 0.928932C43.9526 0.538408 43.3195 0.538408 42.9289 0.928932C42.5384 1.31946 42.5384 1.95262 42.9289 2.34315L48.5858 8L42.9289 13.6569C42.5384 14.0474 42.5384 14.6805 42.9289 15.0711C43.3195 15.4616 43.9526 15.4616 44.3431 15.0711L50.7071 8.70711ZM0 9H50V7H0V9Z" fill="white"/>
                                     </svg>
 
